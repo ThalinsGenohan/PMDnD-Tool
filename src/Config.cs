e9ac8bf9250aee576b32570c2b395f177fa5937b
Thalins Genohan <32170993+ThalinsGenohan@users.noreply.github.com>
2020-03-24 20:48:59 -0400

    Update Config.cs

--- src/Config.cs
+++ src/Config.cs
@@ -6,27 +6,29 @@ using Newtonsoft.Json;
 
 namespace Thalins.PMDnD
 {
-    static class Config
+    internal static class Config
     {
-        const string configFile = "config.json";
+        private const string configFile = "config.json";
 
         private static ConfigObject _cfg;
 
         public static string SpreadsheetID { get => _cfg?.SpreadsheetID; }
 
-        public static void Load()
+        public static bool Load()
         {
             if (!File.Exists(configFile))
             {
                 Console.WriteLine("ERROR! Config file \"{0}\" not found! A new one will be generated for you. Please input the ID of the Google Sheet you are accessing before relaunching.", configFile);
                 File.WriteAllText(configFile, JsonConvert.SerializeObject(_cfg));
+                return false;
             }
 
             _cfg = JsonConvert.DeserializeObject<ConfigObject>(File.ReadAllText(configFile));
+            return true;
         }
     }
 
-    class ConfigObject
+    internal class ConfigObject
     {
         public string SpreadsheetID = "";
     }
